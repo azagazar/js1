ee8482eb85a46beb7d166090de384bf4382bf5da
AgataZarod <a.zarod@alboalbo.com>
2017-03-23 11:09:42 +0100

    wstawienie komentarza

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 var name = prompt('Wpisz swoje imię');
 alert('Witaj, ' + name);
 console.log('Witaj, ' + name);
+
+//tu bedzie alert
+alert('JavaScript jest tutaj !');
+
+//sekcja pierwsza
+//sekcja druga
